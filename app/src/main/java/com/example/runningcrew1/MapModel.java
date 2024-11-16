7efa4277af426c96761b4a1557b86de32f2d6a0a
Jaehoon Yang <lljhms@naver.com>
2024-11-16 22:15:40 +0900

    MapModel 생성

--- app/src/main/java/com/example/runningcrew1/MapModel.java
+++ app/src/main/java/com/example/runningcrew1/MapModel.java
@@ -1,5 +1,19 @@
 package com.example.runningcrew1;
 
 public class MapModel {
+
+    private float displayWidth = 0;
+    private float displayHeight = 0;
+
+    public MapModel(float displayWidth, float displayHeight){
+
+        this.displayWidth = displayWidth;
+        this.displayHeight = displayHeight;
+
+    }
+
+    public void updateposition(){
+
+    }
     // Map-related attributes and methods for handling terrain, etc.
 }
